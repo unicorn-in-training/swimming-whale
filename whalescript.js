dbe09b753d5aa0a0544471ea51693e17e3dcf347
Susie <susaluda@gmail.com>
2017-11-10 09:58:00 -0800

    Optimization
    Combined 2 similar functions into one

--- whalescript.js
+++ whalescript.js
@@ -1,26 +1,33 @@
 window.onload = function() {
-    var debug = true;
+    var debug = false;
 
     var randomNumber = function() {
         var number = Math.round(Math.random() * 50);
         return number;
     };
 
-    var swimmingWhaleRight = function() {
+    var swimmingWhale = function(direction) {
+        direction = direction || "R";
         var debugText = document.getElementById("debug");
         var debugOutput = "";
         if (debug) {
-            debugOutput = debugOutput + "function: swimmingWhaleRight()<br>";
+            debugOutput = debugOutput + "function: swimmingWhale(" + direction + ")<br>";
         }
         var w = window.innerWidth;
         var h = window.innerHeight;
         var elem = document.getElementById("animate");
-        elem.innerHTML = '<img src="whale-r.png" id="whale">';
         var pos = 0;
         var interval = setInterval(frame, 30);
         var sineCalc = 0;
         var x = 0;
         var y = 0;
+        var switchDirection = 0;
+        if (direction == "R") {
+            elem.innerHTML = '<img src="whale-r.png" id="whale">';
+        } else if (direction == "L") {
+            elem.innerHTML = '<img src="whale-l.png" id="whale">';
+            x = w;
+        }
 
         var img = document.getElementById("whale");
         var imgWidth = img.clientWidth;
@@ -33,14 +40,29 @@ window.onload = function() {
                 debugOutput = debugOutput + "x: " + x + "px, ";
                 debugText.innerHTML = debugOutput;
             }
-            if (x + imgWidth >= w) {
+            if (direction == "R") {
+                switchDirection = x + imgWidth >= w;
+            } else if (direction == "L") {
+                switchDirection = x <= imgWidth;
+            }
+
+            if (switchDirection) {
                 clearInterval(interval);
                 // Whale starts swimming from the other side
-                swimmingWhaleLeft();
+                if (direction == "R") {
+                    newDirection = "L";
+                } else if (direction == "L") {
+                    newDirection = "R";
+                }
+                swimmingWhale(newDirection);
             } else {
                 pos++;
                 sineCalc = Math.sin(pos * 0.08);
-                x = pos * 5;
+                if (direction == "R") {
+                    x = pos * 5;
+                } else if (direction == "L") {
+                    x = w - pos * 5;
+                }
                 y = sineCalc * offset + 100;
                 elem.style.top = y + "px";
                 elem.style.left = x + "px";
@@ -48,48 +70,48 @@ window.onload = function() {
         }
     };
 
-    var swimmingWhaleLeft = function() {
-        var debugText = document.getElementById("debug");
-        var debugOutput = "";
-        if (debug) {
-            debugOutput = debugOutput + "function: swimmingWhaleLeft()<br>";
-        }
-        var w = window.innerWidth;
-        var h = window.innerHeight;
-        var elem = document.getElementById("animate");
-        elem.innerHTML = '<img src="whale-l.png" id="whale">';
-        var pos = 0;
-        var interval = setInterval(frame, 30);
-        var sineCalc = 0;
-        var x = w;
-        var y = 0;
+    // var swimmingWhaleLeft = function() {
+    //     var debugText = document.getElementById("debug");
+    //     var debugOutput = "";
+    //     if (debug) {
+    //         debugOutput = debugOutput + "function: swimmingWhaleLeft()<br>";
+    //     }
+    //     var w = window.innerWidth;
+    //     var h = window.innerHeight;
+    //     var elem = document.getElementById("animate");
+    //     elem.innerHTML = '<img src="whale-l.png" id="whale">';
+    //     var pos = 0;
+    //     var interval = setInterval(frame, 30);
+    //     var sineCalc = 0;
+    //     var x = w;
+    //     var y = 0;
+    //
+    //     var img = document.getElementById("whale");
+    //     var imgWidth = img.clientWidth;
+    //     var imgHeight = img.clientHeight;
+    //     var offset = randomNumber();
+    //     console.log(offset);
+    //
+    //     function frame() {
+    //         if (debug) {
+    //             debugOutput = debugOutput + "x: " + x + "px, ";
+    //             debugText.innerHTML = debugOutput;
+    //         }
+    //         if (x <= imgWidth) {
+    //             clearInterval(interval);
+    //             // Whale starts swimming from the other side
+    //             swimmingWhaleRight();
+    //         } else {
+    //             pos++;
+    //             //elem.style.top = pos + 'px';
+    //             sineCalc = Math.sin(pos * 0.08);
+    //             x = w - pos * 5;
+    //             y = sineCalc * offset + 100;
+    //             elem.style.top = y + "px";
+    //             elem.style.left = x + "px";
+    //         }
+    //     }
+    // };
 
-        var img = document.getElementById("whale");
-        var imgWidth = img.clientWidth;
-        var imgHeight = img.clientHeight;
-        var offset = randomNumber();
-        console.log(offset);
-
-        function frame() {
-            if (debug) {
-                debugOutput = debugOutput + "x: " + x + "px, ";
-                debugText.innerHTML = debugOutput;
-            }
-            if (x <= imgWidth) {
-                clearInterval(interval);
-                // Whale starts swimming from the other side
-                swimmingWhaleRight();
-            } else {
-                pos++;
-                //elem.style.top = pos + 'px';
-                sineCalc = Math.sin(pos * 0.08);
-                x = w - pos * 5;
-                y = sineCalc * offset + 100;
-                elem.style.top = y + "px";
-                elem.style.left = x + "px";
-            }
-        }
-    };
-
-    swimmingWhaleRight();
+    swimmingWhale();
 };

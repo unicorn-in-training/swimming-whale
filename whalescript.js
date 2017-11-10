e0fbf7e56448069519383efd3c70b35ca86117f5
Susie <susaluda@gmail.com>
2017-11-10 09:38:46 -0800

    New directions and heights
    Instead of making one pass from left to right, the whale now constantly swims from each side of the page and turns around when it gets to the edge. A randomizer was also added in order to change the height of the sine wave on each pass.

--- whalescript.js
+++ whalescript.js
@@ -1,9 +1,21 @@
 window.onload = function() {
-    function swimmingWhale() {
+    var debug = true;
+
+    var randomNumber = function() {
+        var number = Math.round(Math.random() * 50);
+        return number;
+    };
+
+    var swimmingWhaleRight = function() {
+        var debugText = document.getElementById("debug");
+        var debugOutput = "";
+        if (debug) {
+            debugOutput = debugOutput + "function: swimmingWhaleRight()<br>";
+        }
         var w = window.innerWidth;
         var h = window.innerHeight;
-
         var elem = document.getElementById("animate");
+        elem.innerHTML = '<img src="whale-r.png" id="whale">';
         var pos = 0;
         var interval = setInterval(frame, 30);
         var sineCalc = 0;
@@ -13,21 +25,71 @@ window.onload = function() {
         var img = document.getElementById("whale");
         var imgWidth = img.clientWidth;
         var imgHeight = img.clientHeight;
+        var offset = randomNumber();
+        console.log(offset);
 
         function frame() {
+            if (debug) {
+                debugOutput = debugOutput + "x: " + x + "px, ";
+                debugText.innerHTML = debugOutput;
+            }
             if (x + imgWidth >= w) {
                 clearInterval(interval);
-                elem.innerHTML = "";
+                // Whale starts swimming from the other side
+                swimmingWhaleLeft();
             } else {
                 pos++;
-                //elem.style.top = pos + 'px';
                 sineCalc = Math.sin(pos * 0.08);
                 x = pos * 5;
-                y = sineCalc * 20 + 100;
+                y = sineCalc * offset + 100;
                 elem.style.top = y + "px";
                 elem.style.left = x + "px";
             }
         }
-    }
-    swimmingWhale();
+    };
+
+    var swimmingWhaleLeft = function() {
+        var debugText = document.getElementById("debug");
+        var debugOutput = "";
+        if (debug) {
+            debugOutput = debugOutput + "function: swimmingWhaleLeft()<br>";
+        }
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        var elem = document.getElementById("animate");
+        elem.innerHTML = '<img src="whale-l.png" id="whale">';
+        var pos = 0;
+        var interval = setInterval(frame, 30);
+        var sineCalc = 0;
+        var x = w;
+        var y = 0;
+
+        var img = document.getElementById("whale");
+        var imgWidth = img.clientWidth;
+        var imgHeight = img.clientHeight;
+        var offset = randomNumber();
+        console.log(offset);
+
+        function frame() {
+            if (debug) {
+                debugOutput = debugOutput + "x: " + x + "px, ";
+                debugText.innerHTML = debugOutput;
+            }
+            if (x <= imgWidth) {
+                clearInterval(interval);
+                // Whale starts swimming from the other side
+                swimmingWhaleRight();
+            } else {
+                pos++;
+                //elem.style.top = pos + 'px';
+                sineCalc = Math.sin(pos * 0.08);
+                x = w - pos * 5;
+                y = sineCalc * offset + 100;
+                elem.style.top = y + "px";
+                elem.style.left = x + "px";
+            }
+        }
+    };
+
+    swimmingWhaleRight();
 };

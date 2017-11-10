acfdd168f4e3e2e0a75c382f790d99f845f7ebb5
Susie <susaluda@gmail.com>
2017-11-10 09:59:38 -0800

    Deleted extraneous comments
    Remnants of old function

--- whalescript.js
+++ whalescript.js
@@ -70,48 +70,5 @@ window.onload = function() {
         }
     };
 
-    // var swimmingWhaleLeft = function() {
-    //     var debugText = document.getElementById("debug");
-    //     var debugOutput = "";
-    //     if (debug) {
-    //         debugOutput = debugOutput + "function: swimmingWhaleLeft()<br>";
-    //     }
-    //     var w = window.innerWidth;
-    //     var h = window.innerHeight;
-    //     var elem = document.getElementById("animate");
-    //     elem.innerHTML = '<img src="whale-l.png" id="whale">';
-    //     var pos = 0;
-    //     var interval = setInterval(frame, 30);
-    //     var sineCalc = 0;
-    //     var x = w;
-    //     var y = 0;
-    //
-    //     var img = document.getElementById("whale");
-    //     var imgWidth = img.clientWidth;
-    //     var imgHeight = img.clientHeight;
-    //     var offset = randomNumber();
-    //     console.log(offset);
-    //
-    //     function frame() {
-    //         if (debug) {
-    //             debugOutput = debugOutput + "x: " + x + "px, ";
-    //             debugText.innerHTML = debugOutput;
-    //         }
-    //         if (x <= imgWidth) {
-    //             clearInterval(interval);
-    //             // Whale starts swimming from the other side
-    //             swimmingWhaleRight();
-    //         } else {
-    //             pos++;
-    //             //elem.style.top = pos + 'px';
-    //             sineCalc = Math.sin(pos * 0.08);
-    //             x = w - pos * 5;
-    //             y = sineCalc * offset + 100;
-    //             elem.style.top = y + "px";
-    //             elem.style.left = x + "px";
-    //         }
-    //     }
-    // };
-
     swimmingWhale();
 };
